227127045f7e20023aaaca68f67f565a092a82dc
Brad Ison <brad.ison@redhat.com>
2018-09-07 15:33:01 -0400

    Fix namespace when creating Cluster and MachineSet
    This fixes the namespace on the request when creating the Cluster and
    MachineSet objects to match the target namespace.  It also changes the
    name of secret containing the TLS assets to match what the installer
    is using.

--- cmd/main.go
+++ cmd/main.go
@@ -184,14 +184,14 @@ func deployMachineSet() {
 
 	for {
 		glog.Info("Trying to deploy Cluster object")
-		if _, err := v1alphaClient.Clusters("default").Create(cluster); err != nil {
+		if _, err := v1alphaClient.Clusters(operatorConfig.TargetNamespace).Create(cluster); err != nil {
 			glog.Infof("Cannot create cluster, retrying in 5 secs: %v", err)
 		} else {
 			glog.Info("Created Cluster object")
 		}
 
 		glog.Info("Trying to deploy MachineSet object")
-		_, err = v1alphaClient.MachineSets("default").Create(machineSet)
+		_, err = v1alphaClient.MachineSets(operatorConfig.TargetNamespace).Create(machineSet)
 		if err != nil {
 			glog.Infof("Cannot create MachineSet, retrying in 5 secs: %v", err)
 		} else {
