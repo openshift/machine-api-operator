ab8b51f870c2c849755786837b5a8d5b8a1d9431
vikaschoudhary16 <vichoudh@redhat.com>
2018-09-13 08:25:31 -0400

    vendored packages

--- vendor/github.com/coreos-inc/tectonic-operators/operator-client/pkg/types/secrets.go
+++
@@ -1,7 +0,0 @@
-package types
-
-import "k8s.io/api/core/v1"
-
-// SecretModifier is a modifier function to be used when atomically
-// updating a Secret.
-type SecretModifier func(*v1.Secret) error
