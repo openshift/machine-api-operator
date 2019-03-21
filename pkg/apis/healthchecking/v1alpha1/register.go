ecd51f9534a57192d02232ca2699e4c39072db6c
Artyom Lukianov <alukiano@redhat.com>
2019-03-21 14:53:07 +0200

    Add MachineDisruptionBudget API
    - add api
    - generate deepCopy methods
    - generate clientset

--- pkg/apis/healthchecking/v1alpha1/register.go
+++ pkg/apis/healthchecking/v1alpha1/register.go
@@ -36,6 +36,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&MachineHealthCheck{},
 		&MachineHealthCheckList{},
+		&MachineDisruptionBudget{},
+		&MachineDisruptionBudgetList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
