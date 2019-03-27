05479d8424f8ed20cfce31ecefd116fedbed8459
Jan Chaloupka <jchaloup@redhat.com>
2019-03-27 13:17:48 +0100

    Regen clientset for healthchecking

--- pkg/generated/clientset/versioned/typed/healthchecking/v1alpha1/fake/fake_machinehealthcheck.go
+++ pkg/generated/clientset/versioned/typed/healthchecking/v1alpha1/fake/fake_machinehealthcheck.go
@@ -131,7 +131,7 @@ func (c *FakeMachineHealthChecks) DeleteCollection(options *v1.DeleteOptions, li
 // Patch applies the patch and returns the patched machineHealthCheck.
 func (c *FakeMachineHealthChecks) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.MachineHealthCheck, err error) {
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(machinehealthchecksResource, c.ns, name, data, subresources...), &v1alpha1.MachineHealthCheck{})
+		Invokes(testing.NewPatchSubresourceAction(machinehealthchecksResource, c.ns, name, pt, data, subresources...), &v1alpha1.MachineHealthCheck{})
 
 	if obj == nil {
 		return nil, err
