054ebee9345a32897f83cd32f9d544162e340e3a
Enxebre <alberto.garcial@hotmail.com>
2019-04-01 16:21:22 +0200

    Reuse configv1 for handling platforms

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -177,8 +177,8 @@ func (optr *Operator) sync(key string) error {
 		glog.Errorf("Failed getting operator config: %v", err)
 		return err
 	}
-	if operatorConfig.Controllers.Provider == "None" {
-		glog.V(1).Info("`None` provider specified, reporting available")
+	if operatorConfig.Controllers.Provider == clusterAPIControllerNoOp {
+		glog.V(1).Info("No-op provider specified, reporting available")
 		if err := optr.statusAvailable(); err != nil {
 			glog.Errorf("Error syncing ClusterOperatorStatus: %v", err)
 			return fmt.Errorf("error syncing ClusterOperatorStatus: %v", err)
