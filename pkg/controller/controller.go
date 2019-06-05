233da07b587a3cf338a57724816689043c419263
Enxebre <alberto.garcial@hotmail.com>
2019-06-05 15:27:13 +0200

    Adjust funcs to add controllers per manager with out deps

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,12 +4,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-// AddToManagerFuncs is a list of functions to add all Controllers to the Manager
-var AddToManagerFuncs []func(manager.Manager) error
-
 // AddToManager adds all Controllers to the Manager
-func AddToManager(m manager.Manager) error {
-	for _, f := range AddToManagerFuncs {
+func AddToManager(m manager.Manager, fnList []func(manager.Manager) error) error {
+	for _, f := range fnList {
 		if err := f(m); err != nil {
 			return err
 		}
