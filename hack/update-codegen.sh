9aa8ec9c15d6b643c1867018d1245c2dbc85eece
Brad Ison <brad.ison@redhat.com>
2020-01-14 13:25:54 +0100

    hack: Fix update-codegen and verify-codegen scripts
    This fixes the codegen related scripts to work with the vsphere
    provider API types.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -21,6 +21,10 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
+# The provider types don't need all the extra bits that generate-groups.sh
+# creates.  A simple `go generate` is enough for these.
+go generate ./pkg/apis/vsphereprovider/...
+
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
