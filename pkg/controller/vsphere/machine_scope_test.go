bab132d0ec7146172b9556ca86e946b65e0fc398
Alexander Demichev <ademicev@redhat.com>
2020-03-13 11:49:58 +0100

    Promote vshpere api to beta

--- pkg/controller/vsphere/machine_scope_test.go
+++ pkg/controller/vsphere/machine_scope_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
-	vspherev1 "github.com/openshift/machine-api-operator/pkg/apis/vsphereprovider/v1alpha1"
+	vspherev1 "github.com/openshift/machine-api-operator/pkg/apis/vsphereprovider/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
