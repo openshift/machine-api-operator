c0a96f8e0b80bc01ac0a21b6eb8fe197e2273413
Joel Speed <joel.speed@hotmail.co.uk>
2020-04-03 19:02:54 +0100

    Run Fuzzed RoundTrip test for Machine and MachineSets

--- pkg/apis/machine/v1beta1/v1beta1_suite_test.go
+++ pkg/apis/machine/v1beta1/v1beta1_suite_test.go
@@ -21,7 +21,11 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	fuzz "github.com/google/gofuzz"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeserializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -52,4 +56,123 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 	t.Stop()
 	os.Exit(code)
+}
+
+func machineFuzzerFuncs(codecs runtimeserializer.CodecFactory) []interface{} {
+	return []interface{}{
+		// Fuzzer for pointer to metav1.Time
+		func(j **metav1.Time, c fuzz.Continue) {
+			if c.RandBool() {
+				t := &time.Time{}
+				c.Fuzz(t)
+				*j = &metav1.Time{Time: *t}
+			} else {
+				*j = nil
+			}
+		},
+		// Fuzzer for MachineSpec to ensure empty embedded maps are nil
+		func(j *MachineSpec, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			// Fuzz ObjectMeta using custom fuzzer
+			c.Fuzz(&j.ObjectMeta)
+
+			// Ensure embedded maps are nil if they have zero length
+			if len(j.ObjectMeta.Labels) == 0 {
+				j.ObjectMeta.Labels = nil
+			}
+			if len(j.ObjectMeta.Annotations) == 0 {
+				j.ObjectMeta.Annotations = nil
+			}
+
+			// Ensure slices are nil if they are empty
+			if len(j.Taints) == 0 {
+				j.Taints = nil
+			}
+		},
+		// Fuzzer for MachineStatus to ensure empty embedded maps are nil
+		func(j *MachineStatus, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			// Fuzz LastUpdated using custom fuzzer
+			c.Fuzz(&j.LastUpdated)
+			c.Fuzz(&j.LastOperation)
+
+			// Ensure slices are nil if they are empty
+			if len(j.Addresses) == 0 {
+				j.Addresses = nil
+			}
+		},
+		// Fuzzer for MachineSetSpec to ensure value restrictions are honoured
+		func(j *MachineSetSpec, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			// Fuzz Selector using custom fuzzer
+			c.Fuzz(&j.Selector)
+			if len(j.Selector.MatchLabels) == 0 {
+				j.Selector.MatchLabels = nil
+			}
+			if len(j.Selector.MatchExpressions) == 0 {
+				j.Selector.MatchExpressions = nil
+			}
+
+			// Fuzz Template using custom fuzzers
+			c.Fuzz(&j.Template)
+
+			// Ensure replicas is greater than zero
+			replicas := c.Rand.Int31()
+			j.Replicas = &replicas
+
+			// Set DeletionPolicy to a valid value
+			validDeletionPolicy := []string{
+				string(RandomMachineSetDeletePolicy),
+				string(NewestMachineSetDeletePolicy),
+				string(OldestMachineSetDeletePolicy),
+			}
+			j.DeletePolicy = validDeletionPolicy[c.Rand.Intn(len(validDeletionPolicy))]
+		},
+		// Fuzzer for MachineSetStatus to ensure value restrictions are honoured
+		func(j *MachineSetStatus, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			// Ensure replicas is greater than zero
+			j.Replicas = c.Rand.Int31()
+		},
+		// Fuzzer for ObjectMeta to ensure empty maps are nil
+		func(j *ObjectMeta, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			if len(j.Labels) == 0 {
+				j.Labels = nil
+			} else {
+				delete(j.Labels, "")
+			}
+			if len(j.Annotations) == 0 {
+				j.Annotations = nil
+			} else {
+				delete(j.Annotations, "")
+			}
+			if len(j.OwnerReferences) == 0 {
+				j.OwnerReferences = nil
+			}
+		},
+		// Fuzzer for MachineTemplateSpec to ensure empty embedded maps are nil
+		func(j *MachineTemplateSpec, c fuzz.Continue) {
+			c.FuzzNoCustom(j)
+
+			// Fuzz the ObjectMeta
+			c.Fuzz(&j.ObjectMeta)
+
+			// Ensure embedded maps are nil if they have zero length
+			if len(j.ObjectMeta.Labels) == 0 {
+				j.ObjectMeta.Labels = nil
+			}
+			if len(j.ObjectMeta.Annotations) == 0 {
+				j.ObjectMeta.Annotations = nil
+			}
+
+			// Fuzz the Spec
+			c.Fuzz(&j.Spec)
+		},
+	}
 }
