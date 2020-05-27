498818623859d11f19dd26c14c2305b3b0c34e98
Joel Speed <joel.speed@hotmail.co.uk>
2020-05-27 11:59:29 +0100

    Don't rely on reconcile requests for checking an object has reconciled

--- pkg/controller/machine/machine_controller_test.go
+++ pkg/controller/machine/machine_controller_test.go
@@ -20,24 +20,36 @@ import (
 	"testing"
 	"time"
 
+	. "github.com/onsi/gomega"
 	machinev1beta1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	"golang.org/x/net/context"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
+
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var c client.Client
-
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
 
 const timeout = time.Second * 5
 
 func TestReconcile(t *testing.T) {
 	instance := &machinev1beta1.Machine{
-		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+			Labels: map[string]string{
+				machinev1beta1.MachineClusterIDLabel: "foo",
+			},
+		},
+		Spec: machinev1beta1.MachineSpec{
+			ProviderSpec: machinev1beta1.ProviderSpec{
+				Value: &runtime.RawExtension{
+					Raw: []byte("{}"),
+				},
+			},
+		},
 	}
 
 	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
@@ -49,7 +61,7 @@ func TestReconcile(t *testing.T) {
 	c = mgr.GetClient()
 
 	a := newTestActuator()
-	recFn, requests := SetupTestReconcile(newReconciler(mgr, a))
+	recFn := newReconciler(mgr, a)
 	if err := add(mgr, recFn); err != nil {
 		t.Fatalf("error adding controller to manager: %v", err)
 	}
@@ -67,14 +79,16 @@ func TestReconcile(t *testing.T) {
 		t.Fatalf("error creating instance: %v", err)
 	}
 	defer c.Delete(context.TODO(), instance)
-	select {
-	case recv := <-requests:
-		if recv != expectedRequest {
-			t.Error("received request does not match expected request")
+	g := NewWithT(t)
+	g.Eventually(func() (machinev1beta1.MachineStatus, error) {
+		machine := &machinev1beta1.Machine{}
+		namespacedName := client.ObjectKey{Namespace: instance.Namespace, Name: instance.Name}
+		err := c.Get(ctx, namespacedName, machine)
+		if err != nil {
+			return machinev1beta1.MachineStatus{}, err
 		}
-	case <-time.After(timeout):
-		t.Error("timed out waiting for request")
-	}
+		return machine.Status, nil
+	}, timeout).ShouldNot(Equal(machinev1beta1.MachineStatus{}))
 
 	// TODO: Verify that the actuator is called correctly on Create
 }
