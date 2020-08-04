af2f3fc4d3fc903b92d109cacf1b506362646c4c
Joel Speed <joel.speed@hotmail.co.uk>
2020-08-04 15:26:26 +0100

    Ensure MHC duration fields include units
    Units are mandatory when a time.Duration is being parsed. The regex as 
    it was allowed durations to be optional, this has been updated to force 
    a unit to be included.

--- pkg/apis/machine/v1beta1/machinehealthcheck_types.go
+++ pkg/apis/machine/v1beta1/machinehealthcheck_types.go
@@ -69,7 +69,7 @@ type MachineHealthCheckSpec struct {
 	// Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
 	// +optional
 	// +kubebuilder:default:="10m"
-	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h)*)+$"
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$"
 	// +kubebuilder:validation:Type:=string
 	NodeStartupTimeout metav1.Duration `json:"nodeStartupTimeout,omitempty"`
 }
@@ -89,7 +89,7 @@ type UnhealthyCondition struct {
 	// Expects an unsigned duration string of decimal numbers each with optional
 	// fraction and a unit suffix, eg "300ms", "1.5h" or "2h45m".
 	// Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".
-	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h)*)+$"
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$"
 	// +kubebuilder:validation:Type:=string
 	Timeout metav1.Duration `json:"timeout"`
 }
