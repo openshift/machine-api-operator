67f05d6110e126c69019f2e54fdceec01884f6da
Michael McCune <elmiko@redhat.com>
2022-06-30 12:26:40 -0400

    add utility functions for detecting powershell tags
    This change adds a set of helper functions that will be used with
    Windows machine deployments. When booting a Windows machine, it is
    common for the infrastructure provider to have a method for a Powershell
    script to run during boot. This is usually supplied in the user data
    metadata associated with a new instance. On Azure and AWS, the script is
    required to be wrapped in `<powershell></powershell>` tags, but on GCP
    this is not required. The helper functions in this commit will be used
    by the providers to help in detecting, adding, and removing these tags.

---
+++ pkg/util/windows/helpers.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2022 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package windows
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	powershellOpenTag  = "<powershell>"
+	powershellCloseTag = "</powershell>"
+)
+
+// Return the supplied string wrapped with the powershell tags.
+// If the string already has the tags, do not add them a second a time.
+func AddPowershellTags(target string) string {
+	if HasPowershellTags(target) {
+		return target
+	}
+
+	return fmt.Sprintf("%s%s%s", powershellOpenTag, target, powershellCloseTag)
+}
+
+// Returns true if the string is wrapped with open and close tags for powershell.
+func HasPowershellTags(target string) bool {
+	return strings.HasPrefix(target, powershellOpenTag) && strings.HasSuffix(target, powershellCloseTag)
+}
+
+// Return the supplied string with its powershell tags removed.
+// This function will only remove the tags if both open and close exist,
+// otherwise it returns the original.
+func RemovePowershellTags(target string) string {
+	if !HasPowershellTags(target) {
+		return target
+	}
+
+	target = strings.TrimPrefix(target, powershellOpenTag)
+	target = strings.TrimSuffix(target, powershellCloseTag)
+
+	return target
+}
