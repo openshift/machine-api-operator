7c51a64020e8411e3f737b923e425a428df9a775
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-05-18 16:18:44 -0400

    include IPAM API module

--- tools.go
+++ tools.go
@@ -3,5 +3,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "sigs.k8s.io/cluster-api/exp/ipam/api/v1alpha1"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 )
