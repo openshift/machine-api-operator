3036b2db902f0b39b884bd99b7717f09aa18c2f8
Theo Barber-Bany <tbarberb@redhat.com>
2024-05-21 18:20:03 +0100

    Paused condition for machine
    This change sets the paused condition false when the machine status has
    the AuthoritativeAPI set to MachineAPI, otherwise it sets it to true and
    stops reconciliation.

--- pkg/controller/machine/machine_controller_suite_test.go
+++ pkg/controller/machine/machine_controller_suite_test.go
@@ -18,53 +18,94 @@ package machine
 
 import (
 	"context"
+	"flag"
 	"log"
-	"os"
 	"path/filepath"
 	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	machinev1 "github.com/openshift/api/machine/v1beta1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2/textlogger"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func init() {
+	textLoggerConfig := textlogger.NewConfig()
+	textLoggerConfig.AddFlags(flag.CommandLine)
+	logf.SetLogger(textlogger.NewLogger(textLoggerConfig))
+
+	// Register required object kinds with global scheme.
+	_ = machinev1.Install(scheme.Scheme)
+}
+
+const (
+	timeout = time.Second * 10
 )
 
 var (
-	cfg *rest.Config
-	ctx = context.Background()
+	cfg       *rest.Config
+	ctx       = context.Background()
+	k8sClient client.Client
+	testEnv   *envtest.Environment
 )
 
-func TestMain(m *testing.M) {
-	t := &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "..", "..", "install")},
+func TestMachineController(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "Machine Controller Suite")
+}
+
+var _ = BeforeSuite(func(ctx SpecContext) {
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			Paths: []string{
+				filepath.Join("..", "..", "..", "vendor", "github.com", "openshift", "api", "machine", "v1beta1", "zz_generated.crd-manifests", "0000_10_machine-api_01_machines-CustomNoUpgrade.crd.yaml"),
+			},
+		},
+	}
+
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+})
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	Expect(testEnv.Stop()).To(Succeed())
+})
+
+func StartEnvTest(t *testing.T) func(t *testing.T) {
+	testEnv := &envtest.Environment{
+
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			Paths: []string{
+				filepath.Join("..", "..", "..", "vendor", "github.com", "openshift", "api", "machine", "v1beta1", "zz_generated.crd-manifests", "0000_10_machine-api_01_machines-CustomNoUpgrade.crd.yaml"),
+			},
+		},
 	}
 	if err := machinev1.Install(scheme.Scheme); err != nil {
 		log.Fatalf("cannot add scheme: %v", err)
 	}
 
 	var err error
-	if cfg, err = t.Start(); err != nil {
+	if cfg, err = testEnv.Start(); err != nil {
 		log.Fatal(err)
 	}
 
-	code := m.Run()
-	if err = t.Stop(); err != nil {
-		log.Fatal(err)
+	return func(t *testing.T) {
+		if err = testEnv.Stop(); err != nil {
+			log.Fatal(err)
+		}
 	}
-	os.Exit(code)
-}
-
-// StartTestManager adds recFn
-func StartTestManager(mgr manager.Manager, t *testing.T) (context.CancelFunc, chan error) {
-	t.Helper()
-
-	mgrCtx, cancel := context.WithCancel(ctx)
-	errs := make(chan error, 1)
-
-	go func() {
-		errs <- mgr.Start(mgrCtx)
-	}()
-
-	return cancel, errs
 }
--- pkg/controller/machine/machine_controller_test.go
+++ pkg/controller/machine/machine_controller_test.go
@@ -17,87 +17,200 @@ limitations under the License.
 package machine
 
 import (
-	"testing"
 	"time"
 
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	machinev1 "github.com/openshift/api/machine/v1beta1"
-	"golang.org/x/net/context"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	machinev1 "github.com/openshift/api/machine/v1beta1"
+	"github.com/openshift/machine-api-operator/pkg/util/conditions"
+	"golang.org/x/net/context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
+var _ = Describe("MachineSet Reconciler", func() {
+	var mgrCtxCancel context.CancelFunc
+	var mgrStopped chan struct{}
+	var k komega.Komega
+	var namespace *corev1.Namespace
+	var reconciler reconcile.Reconciler
 
-const timeout = time.Second * 5
-
-func TestReconcile(t *testing.T) {
-	instance := &machinev1.Machine{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "foo",
-			Namespace: "default",
-			Labels: map[string]string{
-				machinev1.MachineClusterIDLabel: "foo",
+	BeforeEach(func() {
+		By("Setting up a new manager")
+		mgr, err := manager.New(cfg, manager.Options{
+			Metrics: server.Options{
+				BindAddress: "0",
 			},
-		},
-		Spec: machinev1.MachineSpec{
-			ProviderSpec: machinev1.ProviderSpec{
-				Value: &runtime.RawExtension{
-					Raw: []byte("{}"),
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		k8sClient = mgr.GetClient()
+		k = komega.New(k8sClient)
+
+		By("Setting up a new reconciler")
+		act := newTestActuator()
+		reconciler = newReconciler(mgr, act)
+
+		err = add(mgr, reconciler, "testing")
+		Expect(err).NotTo(HaveOccurred())
+
+		var mgrCtx context.Context
+		mgrCtx, mgrCtxCancel = context.WithCancel(ctx)
+		mgrStopped = make(chan struct{})
+
+		By("Starting the manager")
+		go func() {
+			defer GinkgoRecover()
+			defer close(mgrStopped)
+
+			Expect(mgr.Start(mgrCtx)).To(Succeed())
+		}()
+
+		By("Creating the namespace")
+		namespace = &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{GenerateName: "ms-test"}}
+		Expect(k8sClient.Create(ctx, namespace)).To(Succeed())
+	})
+
+	AfterEach(func() {
+		By("Deleting the machinesets")
+		Expect(cleanResources()).To(Succeed())
+
+		By("Deleting the namespace")
+		Expect(k8sClient.Delete(ctx, namespace)).To(Succeed())
+
+		By("Closing the manager")
+		mgrCtxCancel()
+		Eventually(mgrStopped, timeout).WithTimeout(20 * time.Second).Should(BeClosed())
+	})
+
+	It("Should reconcile a Machine", func() {
+		instance := &machinev1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "default",
+				Labels: map[string]string{
+					machinev1.MachineClusterIDLabel: "foo",
 				},
 			},
-		},
-	}
+			Spec: machinev1.MachineSpec{
+				ProviderSpec: machinev1.ProviderSpec{
+					Value: &runtime.RawExtension{
+						Raw: []byte("{}"),
+					},
+				},
+			},
+		}
 
-	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// channel when it is finished.
-	mgr, err := manager.New(cfg, manager.Options{
-		Metrics: server.Options{
-			BindAddress: "0",
-		},
+		By("Creating the Machine")
+		Expect(k8sClient.Create(ctx, instance)).To(Succeed())
+
+		By("Setting the AuthoritativeAPI to MachineAPI")
+		Eventually(k.UpdateStatus(instance, func() {
+			instance.Status.AuthoritativeAPI = machinev1.MachineAuthorityMachineAPI
+		})).Should(Succeed())
+
+		Eventually(func() (machinev1.MachineStatus, error) {
+			machine := &machinev1.Machine{}
+			err := k8sClient.Get(ctx, objectKey(instance), machine)
+			if err != nil {
+				return machinev1.MachineStatus{}, err
+			}
+			return machine.Status, nil
+		}, timeout).ShouldNot(Equal(machinev1.MachineStatus{}))
+		// TODO: Verify that the actuator is called correctly on Create
+		// Expect platform status is not empty. This (check) means we've caLLED the actuator
 	})
-	if err != nil {
-		t.Fatalf("error creating new manager: %v", err)
-	}
-	c = mgr.GetClient()
 
-	a := newTestActuator()
-	recFn := newReconciler(mgr, a)
-	if err := add(mgr, recFn, "dummy"); err != nil {
-		t.Fatalf("error adding controller to manager: %v", err)
-	}
-
-	stop, errChan := StartTestManager(mgr, t)
-	defer func() {
-		stop()
-		if err := <-errChan; err != nil {
-			t.Fatalf("error starting test manager: %v", err)
+	It("Should set the Paused condition appropriately", func() {
+		instance := &machinev1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "default",
+				Labels: map[string]string{
+					machinev1.MachineClusterIDLabel: "foo",
+				},
+			},
+			Spec: machinev1.MachineSpec{
+				ProviderSpec: machinev1.ProviderSpec{
+					Value: &runtime.RawExtension{
+						Raw: []byte("{}"),
+					},
+				},
+			},
 		}
-	}()
 
-	// Create the Machine object and expect Reconcile and the actuator to be called
-	if err := c.Create(context.TODO(), instance); err != nil {
-		t.Fatalf("error creating instance: %v", err)
+		By("Creating the Machine")
+		Expect(k8sClient.Create(ctx, instance)).To(Succeed())
+
+		By("Setting the AuthoritativeAPI to ClusterAPI")
+		Eventually(k.UpdateStatus(instance, func() {
+			instance.Status.AuthoritativeAPI = machinev1.MachineAuthorityClusterAPI
+		})).Should(Succeed())
+
+		By("Verifying that the AuthoritativeAPI is set to Cluster API")
+		Eventually(func() (machinev1.MachineAuthority, error) {
+			if err := k8sClient.Get(ctx, objectKey(instance), instance); err != nil {
+				return "", err
+			}
+			return instance.Status.AuthoritativeAPI, nil
+		}, timeout).Should(Equal(machinev1.MachineAuthorityClusterAPI))
+
+		By("Verifying the paused condition is approproately set to true")
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, objectKey(instance), instance); err != nil {
+				return false
+			}
+
+			// The condition is set to true
+			return conditions.IsTrue(instance, PausedCondition)
+		}, timeout).Should(BeTrue())
+
+		By("Transitioning the AuthoritativeAPI though 'Migrating' to MachineAPI")
+		Eventually(k.UpdateStatus(instance, func() {
+			instance.Status.AuthoritativeAPI = machinev1.MachineAuthorityMigrating
+		})).Should(Succeed())
+		Eventually(k.UpdateStatus(instance, func() {
+			instance.Status.AuthoritativeAPI = machinev1.MachineAuthorityMachineAPI
+		})).Should(Succeed())
+
+		By("Verifying the paused condition is approproately set to false")
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, objectKey(instance), instance); err != nil {
+				return false
+			}
+
+			// The condition is set to true
+			return conditions.IsFalse(instance, PausedCondition)
+		}, timeout).Should(BeTrue())
+	})
+})
+
+func objectKey(object metav1.Object) client.ObjectKey {
+	return client.ObjectKey{
+		Namespace: object.GetNamespace(),
+		Name:      object.GetName(),
 	}
-	defer func() {
-		if err := c.Delete(context.TODO(), instance); err != nil {
-			t.Fatalf("error deleting instance: %v", err)
-		}
-	}()
-	g := NewWithT(t)
-	g.Eventually(func() (machinev1.MachineStatus, error) {
-		machine := &machinev1.Machine{}
-		namespacedName := client.ObjectKey{Namespace: instance.Namespace, Name: instance.Name}
-		err := c.Get(ctx, namespacedName, machine)
-		if err != nil {
-			return machinev1.MachineStatus{}, err
-		}
-		return machine.Status, nil
-	}, timeout).ShouldNot(Equal(machinev1.MachineStatus{}))
+}
 
-	// TODO: Verify that the actuator is called correctly on Create
+func cleanResources() error {
+	machines := &machinev1.MachineList{}
+	if err := k8sClient.List(ctx, machines); err != nil {
+		return err
+	}
+	for _, machine := range machines.Items {
+		m := machine
+		if err := k8sClient.Delete(ctx, &m); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
