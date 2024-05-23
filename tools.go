4b3b30fb59521846b30f1f76d3e1e4283a484f82
Anirudh Agnihotri <aagnihot@redhat.com>
2024-05-24 01:21:56 +0530

    Revert "https://issues.redhat.com/browse/OCPBUGS-34391: Revert #1234 "CFE-1051: Add the webhook validation for \"CapacityReservationGroupID\" to \"AzureMachineProviderSpec\" in openshift/machine-api-operator""

--- tools.go
+++ tools.go
@@ -6,5 +6,7 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/machine/v1beta1/zz_generated.crd-manifests"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 )
