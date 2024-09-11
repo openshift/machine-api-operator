3a3b32b728d2489fb260836ebf16de6d44a23013
vr4manta <vr4manta@gmail.com>
2024-09-11 07:28:38 -0400

    Vendor updates

--- vendor/github.com/openshift/api/machine/v1beta1/zz_generated.swagger_doc_generated.go
+++ vendor/github.com/openshift/api/machine/v1beta1/zz_generated.swagger_doc_generated.go
@@ -783,6 +783,16 @@ func (NetworkSpec) SwaggerDoc() map[string]string {
 	return map_NetworkSpec
 }
 
+var map_VSphereDisk = map[string]string{
+	"":           "VSphereDisk describes additional disks for vSphere.",
+	"deviceName": "The device name exposed. Used to identify the disk.",
+	"sizeGb":     "SizeGB is the size of the disk (in GB).",
+}
+
+func (VSphereDisk) SwaggerDoc() map[string]string {
+	return map_VSphereDisk
+}
+
 var map_VSphereMachineProviderSpec = map[string]string{
 	"":                  "VSphereMachineProviderSpec is the type that will be embedded in a Machine.Spec.ProviderSpec field for an VSphere virtual machine. It is used by the vSphere machine actuator to create a single Machine. Compatibility level 2: Stable within a major release for a minimum of 9 months or 3 minor releases (whichever is longer).",
 	"userDataSecret":    "UserDataSecret contains a local reference to a secret that contains the UserData to apply to the instance",
@@ -797,6 +807,7 @@ var map_VSphereMachineProviderSpec = map[string]string{
 	"tagIDs":            "tagIDs is an optional set of tags to add to an instance. Specified tagIDs must use URN-notation instead of display names. A maximum of 10 tag IDs may be specified.",
 	"snapshot":          "Snapshot is the name of the snapshot from which the VM was cloned",
 	"cloneMode":         "CloneMode specifies the type of clone operation. The LinkedClone mode is only support for templates that have at least one snapshot. If the template has no snapshots, then CloneMode defaults to FullClone. When LinkedClone mode is enabled the DiskGiB field is ignored as it is not possible to expand disks of linked clones. Defaults to FullClone. When using LinkedClone, if no snapshots exist for the source template, falls back to FullClone.",
+	"disks":             "Disks is a list of non OS disks to be attached to the VM.",
 }
 
 func (VSphereMachineProviderSpec) SwaggerDoc() map[string]string {
